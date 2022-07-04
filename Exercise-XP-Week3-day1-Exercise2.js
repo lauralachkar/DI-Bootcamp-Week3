2f7c321fcb03cfe8debe49484e5812eddc2ec74f
lauralachkar <99486645+lauralachkar@users.noreply.github.com>
2022-07-04 17:29:53 +0300

    Add files via upload

---
+++ Exercise-XP-Week3-day1-Exercise2.js
@@ -0,0 +1,17 @@
+document.getElementsByTagName("div")[0].setAttribute
+ ("style", "background-color: blue;")
+
+document.getElementsByTagName("div")[0].style.padding = "20px";
+
+let deleteValues = document.querySelectorAll("ul li");
+  for(i = 0 ; (li = deleteValues[i]); i++){
+    if(deleteValues[i].innerHTML == "John"){
+     li.parentNode.removeChild(li)
+    }
+}
+
+
+document.getElementsByTagName("li")[0].setAttribute("style", "border: solid black 1px");
+
+
+document.getElementsByTagName("body")[0].style.fontSize =  "x-large";
---
+++ Exercise-XP-week3-day1-Exercise1.js
@@ -0,0 +1,25 @@
+//Exercise 1
+
+console.log(document.getElementsByTagName("div"))
+
+ let listItems = document.querySelectorAll("li");
+ for(i=0; i<listItems.length; i++){
+  if(listItems[i].innerHTML == "Pete"){
+   listItems[i].innerHTML = "Richard";
+  }
+ }
+
+
+ let listOfItems = document.querySelectorAll(".list");
+ for(i=0; i<listOfItems.length; i++){
+  listOfItems[i].firstElementChild.innerHTML = "laura";
+ }
+
+
+ let listElements = document.querySelectorAll(".list li");
+  for (let i = 0; (li = listElements[i]); i++) {
+   if(listElements[i].innerHTML == "Sarah")
+   li.parentNode.removeChild(li);
+  }
+
+
