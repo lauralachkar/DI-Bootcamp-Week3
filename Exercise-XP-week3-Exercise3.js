e3cd40883515fa835d844597a0df527b9697ec0b
lauralachkar <99486645+lauralachkar@users.noreply.github.com>
2022-07-04 17:28:49 +0300

    Delete Exercise-XP-week3-Exercise3.js

--- Exercise-XP-week3-Exercise3.js
+++
@@ -1,8 +0,0 @@
-let changeId = document.getElementById("navBar");
-changeId.id = "socialNetworkNavigation"
-
- let newLI = document.createElement("li");
- let newLiText = "Logout";
- newLI.innerHTML = newLiText;
- document.getElementsByTagName("ul")[0].appendChild(newLI);
-
